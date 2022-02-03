736882820b4b15b3e0e9922ad6f2244bb868c00b
bcat <v01z@mail.ru>
2022-02-03 22:15:45 +0300

    Ready.

---
+++ QtLanguage_en.ts
@@ -0,0 +1,321 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>KeyBindDialog</name>
+    <message>
+        <location filename="keybinddialog.ui" line="14"/>
+        <source>Настройка &quot;горячих клавиш&quot;</source>
+        <translation type="unfinished">Hot keys binding</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.ui" line="36"/>
+        <source>Действие:</source>
+        <translation type="unfinished">Action:</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.ui" line="49"/>
+        <source>Комбинация клавиш</source>
+        <translation type="unfinished">Keys combinations</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.ui" line="91"/>
+        <source>Модификатор 1:</source>
+        <translation type="unfinished">Mod 1:</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.ui" line="104"/>
+        <source>Модификатор 2:</source>
+        <translation type="unfinished">Mod 2:</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.ui" line="117"/>
+        <source>Клавиша-ключ:</source>
+        <translation type="unfinished">Key:</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.ui" line="131"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.ui" line="144"/>
+        <source>Отмена</source>
+        <translation type="unfinished">Cancel</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="20"/>
+        <source>MainWindow</source>
+        <translation type="unfinished">Homework 4</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="40"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="55"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="59"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="69"/>
+        <source>?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="80"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="85"/>
+        <source>Open read only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="90"/>
+        <source>SaveAs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="95"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="100"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="105"/>
+        <source>Key bindings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="110"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="118"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="129"/>
+        <location filename="mainwindow.cpp" line="46"/>
+        <source>Русский</source>
+        <translation type="unfinished">Russian</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="137"/>
+        <location filename="mainwindow.cpp" line="47"/>
+        <source>Английский</source>
+        <translation type="unfinished">English</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="33"/>
+        <source>Домашняя работа №4</source>
+        <translation type="unfinished">Homework 4</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="35"/>
+        <source>Файл</source>
+        <translation type="unfinished">File</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="36"/>
+        <source>Открыть</source>
+        <translation type="unfinished">Open</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="37"/>
+        <source>Открыть только для чтения</source>
+        <translation type="unfinished">Open read only</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="38"/>
+        <source>Закрыть</source>
+        <translation type="unfinished">Close</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="39"/>
+        <location filename="mainwindow.cpp" line="217"/>
+        <source>Сохранить</source>
+        <translation type="unfinished">Save</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="40"/>
+        <source>Сохранить как</source>
+        <translation type="unfinished">Save as</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="41"/>
+        <source>Выход</source>
+        <translation type="unfinished">Exit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="43"/>
+        <source>Настройки</source>
+        <translation type="unfinished">Settings</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="45"/>
+        <source>Язык</source>
+        <translation type="unfinished">Language</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="49"/>
+        <source>Сочетания клавиш</source>
+        <translation type="unfinished">Hot keys</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="51"/>
+        <source>Помощь</source>
+        <translation type="unfinished">Help</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="135"/>
+        <source> открыт в режиме &apos;только для чтения&apos;.</source>
+        <translation type="unfinished"> is opened in read only mode.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="157"/>
+        <source> сохранён.</source>
+        <translation type="unfinished"> has been saved.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="177"/>
+        <source>Сохранить файл как</source>
+        <translation type="unfinished">Save file as</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="192"/>
+        <source>Файл сохранён как </source>
+        <translation type="unfinished">File saved as </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="212"/>
+        <source>Текст изменён.</source>
+        <translation type="unfinished">Text has been changed.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="213"/>
+        <source>Хотите его сохранить?</source>
+        <translation type="unfinished">Do you want to save it?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="218"/>
+        <source>Отмена</source>
+        <translation type="unfinished">Cancel</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="219"/>
+        <source>Выйти без сохранения</source>
+        <translation type="unfinished">Discard</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="249"/>
+        <source>Текстовый редактор с возможностью переключения языка интерфейса.</source>
+        <translation type="unfinished">Text editor. Interface can be translated to next languages  Russian and English.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="252"/>
+        <source>Краткая справка показана.</source>
+        <translation type="unfinished">Short hint has been shown.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="keybinddialog.h" line="41"/>
+        <source>Открыть</source>
+        <translation type="unfinished">Open</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.h" line="42"/>
+        <source>Русский</source>
+        <translation type="unfinished">Russian</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.h" line="43"/>
+        <source>Открыть для чтения</source>
+        <translation type="unfinished">Open read only</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.h" line="44"/>
+        <source>Закрыть</source>
+        <translation type="unfinished">Close</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.h" line="45"/>
+        <source>Сохранить как</source>
+        <translation type="unfinished">Save as</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.h" line="46"/>
+        <source>Сохранить</source>
+        <translation type="unfinished">Save</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.h" line="47"/>
+        <source>Выход</source>
+        <translation type="unfinished">Exit</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.h" line="48"/>
+        <source>Английский</source>
+        <translation type="unfinished">English</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.h" line="49"/>
+        <source>Помощь</source>
+        <translation type="unfinished">Help</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.h" line="50"/>
+        <source>Забиндить клавиши</source>
+        <translation type="unfinished">Hot keys</translation>
+    </message>
+    <message>
+        <location filename="keybinddialog.h" line="57"/>
+        <source>Пусто</source>
+        <translation type="unfinished">Empty</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="5"/>
+        <source>Файл не найден</source>
+        <translation type="unfinished">File not found</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="6"/>
+        <source>Текстовый файл(*.txt)</source>
+        <translation type="unfinished">Text file(*.txt)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="7"/>
+        <source>Выберите файл для открытия</source>
+        <translation type="unfinished">Choose file to open</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="8"/>
+        <source>Не могу открыть файл </source>
+        <translation type="unfinished">Cant open file </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="9"/>
+        <source>Файл </source>
+        <translation type="unfinished">File </translation>
+    </message>
+</context>
+</TS>
